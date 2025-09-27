e09a3ba8203f504f9f774e0cf0a61419aa8ae0c6
sandeep <sandeepky567@gmail.com>
2025-09-27 15:39:41 +0530

    new changes

--- models/listing.js
+++ models/listing.js
@@ -15,7 +15,7 @@ const listingSchema = new Schema({
      
         set:(v)=> v===''?'https://images.unsplash.com/photo-1585245793787-ba3b6539f98d?q=80&w=712&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         :v
-       
+          
     },
 
     price: Number,
